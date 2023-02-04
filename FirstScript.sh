520c07b68955dadb595ba1769451c0fa06a3760e
oktoknopi <124457248+oktoknopi@users.noreply.github.com>
2023-02-04 18:12:44 +0100

    "it's an update"
    just trying to make strings

--- FirstScript.sh
+++ FirstScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Hello World! 
-echo Wow, this really works
-echo What now?! More edits?.
-echo Ah, I saw something I needed to change! How handy!
-echo Dit is een voorbeeld
+echo "Hello World!
+Perhaps this also works?"
+echo "What now?! More edits?."
+echo "Ah, I saw something I needed to change! How handy!"
+echo "Dit is een voorbeeld"
